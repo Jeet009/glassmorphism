3b59ac645dff0ccce9544834f64d1dfa8a90595f
Jeet009 <jeetmukherjee009@gmail.com>
2020-12-26 19:42:14 +0530

    finished first design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import bruno from "./assets/cyberpunk.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="background row">
+        <div className="bg-left col-md-6"></div>
+        <div className="bg-right col-md-6"></div>
+      </div>
+      <div className="card-container">
+        <div className="card">
+          <div className="row">
+            <div className="col-md-6 card-details">
+              <div>
+                <h2>Cyberpunk 2077</h2>
+                <p>
+                  Lorem ipsum, or lipsum as it is sometimes known,
+                  <br /> is dummy text used in laying out print, graphic or web
+                  designs.
+                </p>
+                <button className="btn btn-custom">Purchase Now</button>
+              </div>
+            </div>
+            <div className="col-md-6 card-img">
+              <img src={bruno} className="img-fluid" alt="..." />
+            </div>
+          </div>
+          <footer className="footer">
+            <div className="footer-container">
+              <span className="fa fa-facebook-square"></span>
+              <span className="fa fa-instagram"></span>
+              <span className="fa fa-download"></span>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 }
 
